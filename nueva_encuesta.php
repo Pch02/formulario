80c69cac1789379519a5eed42f5d2081c7773214
pablo <pablocumbreras1@gmail.com>
2020-03-14 18:00:24 +0100

    nuevos php

--- nueva_encuesta.php
+++ nueva_encuesta.php
@@ -2,4 +2,30 @@
 <head>
     <title>Nueva encuesta</title>
 </head>
+<body>
+<header>
+    Encuesta de opinión de los/as estudiantes sobre la labor docente del profesorado
+</header>
+<br>
+<nav>
+    <h1>Código de la asignatura</h1>
+    <form METHOD="POST" ACTION=
+
+
+
+
+
+
+
+</nav>
+
+
+
+
+
+
+
+
+
+</body>
 </html>
