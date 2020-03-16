e621983c7d579468732824a5008354c015181058
Pedro <pedro.campoybarrera@alum.uca.es>
2020-03-16 20:12:23 +0100

    Actualización de prueba

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </nav>
 
 <footer>
-    <img src="images/logouca.png" boder="1" width="100" height="100">
+    <img src="images/logouca.png" width="100" height="100" alt="Suni" class="center">
 </footer>
 <?php
 ?>
